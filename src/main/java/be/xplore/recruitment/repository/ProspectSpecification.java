2243b8bd2bf58dc6276589885aee57d8d1a7b9f2
Stijn Schack <stijn.schack@student.kdg.be>
2017-07-20 10:13:14 +0200

    added specification class and methods

--- src/main/java/be/xplore/recruitment/repository/ProspectSpecification.java
+++ src/main/java/be/xplore/recruitment/repository/ProspectSpecification.java
@@ -3,32 +3,23 @@ package be.xplore.recruitment.repository;
 import be.xplore.recruitment.model.Prospect;
 import org.springframework.data.jpa.domain.Specification;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-
 /**
  * @author Stijn Schack
  * @since 7/20/2017
  */
 public class ProspectSpecification {
-    //TODO: Stijn
     public static Specification<Prospect> hasFirstName(String firstName) {
-        return null;
+        return (root, query, cb) -> cb.equal(root.get("firstName"), (firstName));
     }
 
-    //TODO: Stijn
     public static Specification<Prospect> hasLastName(String lastName) {
-        return null;
+        return (root, query, cb) -> cb.equal(root.get("lastName"), (lastName));
     }
 
-    //TODO: Lander
     public static Specification<Prospect> hasEmail(String email) {
         return (root, query, cb) -> cb.equal(root.get("email"), email);
     }
 
-    //TODO: Lander
     public static Specification<Prospect> hasPhone(String phone) {
         return (root, query, cb) -> cb.equal(root.get("phone"), phone);
     }
