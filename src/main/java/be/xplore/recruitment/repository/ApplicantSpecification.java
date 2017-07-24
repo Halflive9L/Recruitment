4c6103a375170613a83f65174a7deb561d4ce65b
Lander <lander.tuyteleers@gmail.com>
2017-07-24 09:09:44 +0200

    Date rest call (YYYY-MM-DD)

--- src/main/java/be/xplore/recruitment/repository/ApplicantSpecification.java
+++ src/main/java/be/xplore/recruitment/repository/ApplicantSpecification.java
@@ -4,13 +4,17 @@ package be.xplore.recruitment.repository;
 import be.xplore.recruitment.domain.model.Applicant;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.util.Date;
+import java.sql.Date;
+import java.util.Calendar;
 
 /**
  * Created by Lander on 20/07/2017.
@@ -41,8 +45,23 @@ public class ApplicantSpecification {
     }
 
     public static Specification<Applicant> hasDateOfBirth(String dateOfBirth) {
-        System.out.println(dateOfBirth);
-        return (root, query, cb) -> cb.equal(root.get("dateOfBirth"), dateOfBirth);
+        return (root, query, cb) -> {
+            SimpleDateFormat formatter = new SimpleDateFormat("YYYY-MM-DD");
+            java.util.Date date = null;
+            try {
+                date = formatter.parse(dateOfBirth);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(date);
+            cal.add(Calendar.DATE, 1);
+            Date lowDate = (Date)cal.getTime();
+            cal.setTime(date);
+            cal.add(Calendar.DATE, -1);
+            Date highDate = (Date)cal.getTime();
+            return cb.between(root.get("dateOfBirth"), lowDate, highDate);
+        };
     }
 
 
