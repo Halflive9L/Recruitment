60921f31e05ad157a2c56b028ce75115952fc07f
Lander <lander.tuyteleers@gmail.com>
2017-07-26 14:50:02 +0200

    Boundaries

---
+++ domain/src/main/java/be/xplore/recruitment/domain/prospect/CreateProspectRequest.java
@@ -0,0 +1,11 @@
+package be.xplore.recruitment.domain.prospect;
+
+import java.util.List;
+
+/**
+ * Created by Lander on 26/07/2017.
+ */
+public class CreateProspectRequest {
+    public List<Prospect> prospects;
+
+}
