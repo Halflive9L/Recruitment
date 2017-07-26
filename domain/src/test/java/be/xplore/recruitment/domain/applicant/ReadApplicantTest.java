18250e3eeabb3c6894fb99c48be43b3fd61545e1
Stijn Schack <stijn.schack@student.kdg.be>
2017-07-26 14:29:26 +0200

    read prospect

---
+++ domain/src/test/java/be/xplore/recruitment/domain/applicant/ReadApplicantTest.java
@@ -0,0 +1,13 @@
+package be.xplore.recruitment.domain.applicant;
+
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * @author Stijn Schack
+ * @since 7/26/2017
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ReadApplicantTest {
+
+}
