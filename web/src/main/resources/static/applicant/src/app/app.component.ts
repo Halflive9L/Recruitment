c7be7a02fc7f328296c9c7820c7cd62b4a761c48
Lander <lander.tuyteleers@gmail.com>
2017-08-04 09:39:34 +0200

    Client side validatie van Prospect (firstname, lastname, email & phone)

--- web/src/main/resources/static/applicant/src/app/app.component.ts
+++ web/src/main/resources/static/applicant/src/app/app.component.ts
@@ -24,6 +24,7 @@ export   class   AppComponent  {
   }
 
   applicantForm:  FormGroup;
+  prospectForm: FormGroup;
 
   ngOnInit() {
     this.applicantForm = this.formBuilder.group({
@@ -40,6 +41,17 @@ export   class   AppComponent  {
       education: ''
     });
 
+    this.prospectForm = this.formBuilder.group({
+      firstName : ['', [Validators.required, Validators.minLength(2),Validators.maxLength(50)]],
+      lastName: ['', [Validators.required,Validators.minLength(2),Validators.maxLength(50)]],
+      email: ['',[Validators.required, Validators.pattern("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+" +
+        "@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])" +
+        "|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$")]],
+      phone: ['',[Validators.required,Validators.pattern("\\+(9[976]\\d|8[987530]\\d|6[987]\\d|5[90]\\d|42\\d|" +
+        "3[875]\\d|2[98654321]\\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|" +
+        "4[987654310]|3[9643210]|2[70]|7|1)\\d{1,14}")]]
+    });
+
     this._applicant.getApplicants()
       .subscribe(iapplicants => this.iapplicants = iapplicants);
     this._prospects.getProspects()
@@ -52,8 +64,13 @@ export   class   AppComponent  {
     let body = JSON.stringify(form.value);
     this._applicant.createApplicant(body)
       .subscribe(iapplicant => {
+        if(this.iapplicants.length == 0) {
+          iapplicant[0].applicantId = 1;
+        } else {
         iapplicant[0].applicantId = this.iapplicants.length+1;
+        }
         this.iapplicants.push(iapplicant[0])
+
       });
   };
 
@@ -61,7 +78,11 @@ export   class   AppComponent  {
     let body = JSON.stringify(form.value);
     this._prospects.createProspect(body)
       .subscribe(iprospect => {
+        if(this.iprospects.length == 0) {
+          iprospect[0].prospectId = 1;
+        } else {
         iprospect[0].prospectId = this.iprospects.length+1;
+        }
         this.iprospects.push(iprospect[0])
       });
   };
