dd392440d42aa29f29b543b088026c7f36d81d64
Lander <lander.tuyteleers@gmail.com>
2017-08-07 09:14:32 +0200

    Fixed last todo: Create now takes the highest possible id.

--- web/src/main/resources/static/applicant/src/app/app.component.ts
+++ web/src/main/resources/static/applicant/src/app/app.component.ts
@@ -1,11 +1,11 @@
-import {Component, ViewChild, ElementRef} from '@angular/core';
+import {Component} from '@angular/core';
 import {IApplicant} from "./applicants";
 import {ApplicantsService} from "./applicants.service";
-import {FormGroup, FormBuilder, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {ProspectsService} from "./prospects.service";
 import {IProspect} from "./prospects";
-import {Observable} from "rxjs/Observable";
 import {forEach} from "@angular/router/src/utils/collection";
+import {postApplicantComponent} from "./postApplicant.component";
 
 
 @Component({
@@ -15,13 +15,16 @@ import {forEach} from "@angular/router/src/utils/collection";
   providers: [ApplicantsService, ProspectsService]
 })
 
-export   class   AppComponent  {
+export class AppComponent {
   iapplicants: IApplicant[];
   iapplicant: IApplicant;
   iprospects: IProspect[];
   iprospect: IProspect;
-  currentId: number;
-  applicantForm:  FormGroup;
+  currentApplicantId: number;
+  currentProspectId: number;
+  highestApplicantId: number = 0;
+  highestProspectId: number = 0;
+  applicantForm: FormGroup;
   prospectForm: FormGroup;
   updateApplicantForm: FormGroup;
   updateProspectForm: FormGroup;
@@ -31,15 +34,14 @@ export   class   AppComponent  {
   }
 
 
-
   ngOnInit() {
     this.applicantForm = this.formBuilder.group({
       firstName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
       lastName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
-      email: ['',[Validators.required, Validators.pattern("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+" +
+      email: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+" +
         "@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])" +
         "|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$")]],
-      phone: ['',[Validators.required,Validators.pattern("\\+(9[976]\\d|8[987530]\\d|6[987]\\d|5[90]\\d|42\\d|" +
+      phone: ['', [Validators.required, Validators.pattern("\\+(9[976]\\d|8[987530]\\d|6[987]\\d|5[90]\\d|42\\d|" +
         "3[875]\\d|2[98654321]\\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|" +
         "4[987654310]|3[9643210]|2[70]|7|1)\\d{1,14}")]],
       address: '',
@@ -50,10 +52,10 @@ export   class   AppComponent  {
     this.updateApplicantForm = this.formBuilder.group({
       firstName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
       lastName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
-      email: ['',[Validators.required, Validators.pattern("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+" +
+      email: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+" +
         "@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])" +
         "|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$")]],
-      phone: ['',[Validators.required,Validators.pattern("\\+(9[976]\\d|8[987530]\\d|6[987]\\d|5[90]\\d|42\\d|" +
+      phone: ['', [Validators.required, Validators.pattern("\\+(9[976]\\d|8[987530]\\d|6[987]\\d|5[90]\\d|42\\d|" +
         "3[875]\\d|2[98654321]\\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|" +
         "4[987654310]|3[9643210]|2[70]|7|1)\\d{1,14}")]],
       address: '',
@@ -62,120 +64,126 @@ export   class   AppComponent  {
     });
 
     this.prospectForm = this.formBuilder.group({
-      firstName : ['', [Validators.required, Validators.minLength(2),Validators.maxLength(50)]],
-      lastName: ['', [Validators.required,Validators.minLength(2),Validators.maxLength(50)]],
-      email: ['',[Validators.required, Validators.pattern("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+" +
+      firstName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+      lastName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+" +
         "@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])" +
         "|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$")]],
-      phone: ['',[Validators.required,Validators.pattern("\\+(9[976]\\d|8[987530]\\d|6[987]\\d|5[90]\\d|42\\d|" +
+      phone: ['', [Validators.required, Validators.pattern("\\+(9[976]\\d|8[987530]\\d|6[987]\\d|5[90]\\d|42\\d|" +
         "3[875]\\d|2[98654321]\\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|" +
         "4[987654310]|3[9643210]|2[70]|7|1)\\d{1,14}")]]
     });
 
     this.updateProspectForm = this.formBuilder.group({
-      firstName : ['', [Validators.required, Validators.minLength(2),Validators.maxLength(50)]],
-      lastName: ['', [Validators.required,Validators.minLength(2),Validators.maxLength(50)]],
-      email: ['',[Validators.required, Validators.pattern("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+" +
+      firstName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+      lastName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+" +
         "@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])" +
         "|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$")]],
-      phone: ['',[Validators.required,Validators.pattern("\\+(9[976]\\d|8[987530]\\d|6[987]\\d|5[90]\\d|42\\d|" +
+      phone: ['', [Validators.required, Validators.pattern("\\+(9[976]\\d|8[987530]\\d|6[987]\\d|5[90]\\d|42\\d|" +
         "3[875]\\d|2[98654321]\\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|" +
         "4[987654310]|3[9643210]|2[70]|7|1)\\d{1,14}")]]
     });
 
     this._applicant.getApplicants()
-      .subscribe(iapplicants => this.iapplicants = iapplicants);
+      .subscribe(iapplicants => {
+        iapplicants.forEach(iapplicant =>  this.highestApplicantId = iapplicant.applicantId);
+        this.iapplicants = iapplicants });
     this._prospects.getProspects()
-      .subscribe(iprospects => this.iprospects = iprospects);
+      .subscribe(iprospects => {
+        iprospects.forEach(iprospect => this.highestProspectId = iprospect.prospectId);
+      this.iprospects = iprospects; });
   };
 
-  getId($event) {
-    if(isNaN($event.target.id)) {
-      this.currentId = 0;
+  getApplicantId($event) {
+    if (isNaN($event.target.id)) {
+      this.currentApplicantId = 0;
     } else {
-    this.currentId = $event.target.id;
+      this.currentApplicantId = $event.target.id;
     }
   };
 
-  readApplicantById(id: number) : IApplicant {
-    if(isNaN(id)) return null;
+  getProspectId($event) {
+    if (isNaN($event.target.id)) {
+      this.currentProspectId = 0;
+    } else {
+      this.currentProspectId = $event.target.id;
+    }
+  };
+
+  readApplicantById(id: number): IApplicant {
+    if (isNaN(id)) return null;
     return this.iapplicants[id];
   };
 
-  addApplicant(form: FormGroup) : void {
+  addApplicant(form: FormGroup): void {
     let body = JSON.stringify(form.value);
     this._applicant.createApplicant(body)
       .subscribe(iapplicant => {
-        if(this.iapplicants.length == 0) {
-          iapplicant[0].applicantId = 1;
-        } else {
-        iapplicant[0].applicantId = this.iapplicants.length+1;
-        }
+          iapplicant[0].applicantId = this.highestApplicantId + 1;
+          this.highestApplicantId++;
         this.iapplicants.push(iapplicant[0])
-
       });
   };
 
-  updateApplicant(form: FormGroup) : void {
+  updateApplicant(form: FormGroup): void {
     let body = JSON.stringify(form.value);
-    this._applicant.updateApplicant(body, this.currentId)
+    this._applicant.updateApplicant(body, this.currentApplicantId)
       .subscribe(iapplicant => {
-          console.log("lenght = " + this.iapplicants.length);
-          console.log("id = " + this.currentId)
-          let index = this.iapplicants.findIndex(x => x.applicantId === iapplicant[0].applicantId);
+        console.log("lenght = " + this.iapplicants.length);
+        console.log("id = " + this.currentApplicantId)
+        let index = this.iapplicants.findIndex(x => x.applicantId === iapplicant[0].applicantId);
         let array1 = this.iapplicants.slice(0, index);
-        let array2 = this.iapplicants.slice(index+1, this.iapplicants.length);
+        let array2 = this.iapplicants.slice(index + 1, this.iapplicants.length);
         array1.push(iapplicant[0]);
         this.iapplicants = array1.concat(array2);
-    });
-    };
+      });
+  };
 
-    deleteApplicant(id: number) : void {
+  deleteApplicant(id: number): void {
     this._applicant.deleteApplicant(id)
       .subscribe(iapplicant => {
         let index = this.iapplicants.findIndex(x => x.applicantId === iapplicant[0].applicantId);
-          let array1 = this.iapplicants.slice(0, index);
-          let array2 = this.iapplicants.slice(index+1, this.iapplicants.length);
-          this.iapplicants = array1.concat(array2);
-    });
-    };
+        let array1 = this.iapplicants.slice(0, index);
+        let array2 = this.iapplicants.slice(index + 1, this.iapplicants.length);
+        this.iapplicants = array1.concat(array2);
+      });
+  };
 
-  readProspectById(id: number) : IProspect {
-    if(isNaN(id)) return null;
+  readProspectById(id: number): IProspect {
+    if (isNaN(id)) return null;
     return this.iprospects[id];
   };
 
-  addProspect(form: FormGroup) : void {
+  addProspect(form: FormGroup): void {
     let body = JSON.stringify(form.value);
     this._prospects.createProspect(body)
       .subscribe(iprospect => {
-        if(this.iprospects.length == 0) {
-          iprospect[0].prospectId = 1;
-        } else {
-        iprospect[0].prospectId = this.iprospects.length+1;
-        }
+          iprospect[0].prospectId = this.highestProspectId + 1;
+          this.highestProspectId++;
         this.iprospects.push(iprospect[0])
       });
   };
 
-  updateProspect(form: FormGroup) : void {
+  updateProspect(form: FormGroup): void {
     let body = JSON.stringify(form.value);
-    this._prospects.updateProspect(body, this.currentId)
+    this._prospects.updateProspect(body, this.currentProspectId)
       .subscribe(iprospect => {
         let index = this.iprospects.findIndex(x => x.prospectId === iprospect[0].prospectId);
         let array1 = this.iprospects.slice(0, index);
-        let array2 = this.iprospects.slice(index+1, this.iprospects.length);
+        let array2 = this.iprospects.slice(index + 1, this.iprospects.length);
         array1.push(iprospect[0]);
         this.iprospects = array1.concat(array2);
       });
-  }
+  };
 
-  deleteProspect(id: number) : void {
+  deleteProspect(id: number): void {
+    console.log(id);
     this._prospects.deleteProspect(id)
       .subscribe(iprospect => {
         let index = this.iprospects.findIndex(x => x.prospectId === iprospect[0].prospectId);
         let array1 = this.iprospects.slice(0, index);
-        let array2 = this.iprospects.slice(index+1, this.iprospects.length);
+        let array2 = this.iprospects.slice(index + 1, this.iprospects.length);
         this.iprospects = array1.concat(array2);
       });
   };
