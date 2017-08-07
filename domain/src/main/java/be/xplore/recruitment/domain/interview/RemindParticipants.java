2abaad12356a51b35839a15df039e18b3901d022
mkb <mkb@xd.xd>
2017-08-07 12:09:52 +0200

    Reminder tests start

--- domain/src/main/java/be/xplore/recruitment/domain/interview/RemindParticipants.java
+++ domain/src/main/java/be/xplore/recruitment/domain/interview/RemindParticipants.java
@@ -1,12 +1,12 @@
 package be.xplore.recruitment.domain.interview;
 
 public interface RemindParticipants {
-    void checkInterviews();
+    //List<Interview> getInterviewsToRemind();
+    //void sendReminders(Interview interview);
+    void checkReminders();
 
     // 3. markeren interview als reminder verzonden
     //      -> ++veld in interview (preInterviewReminderSent)
-
-
 
     // 1. fetch alle interviews die geremind moete worde uit db
     //      SELECT .. WHERE reminderSent = false AND scheduledTime < (NOW-24h)
@@ -14,5 +14,5 @@ public interface RemindParticipants {
     // 2. send emails (interviewers, applicant)
     //      -> CLEAN arch boundary -> SMTP implementatie
     // 4. Spring @Scheduled voor periodiek check
-    //      -> Deze UC...
+    //      -> Deze UC callen
 }
--- domain/src/test/java/be/xplore/recruitment/domain/interview/ReminderTest.java
+++ domain/src/test/java/be/xplore/recruitment/domain/interview/ReminderTest.java
@@ -10,47 +10,136 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 
 @RunWith(MockitoJUnitRunner.class)
 public class ReminderTest {
     @Mock
     private MockInterviewRepo mockInterviewRepo;
+    private RemindParticipants useCase;
+
+    private List<Applicant> applicants = Arrays.asList(
+            Applicant.builder()
+                    .withId(1)
+                    .withEmail("CaseyStarrenburg@dayrep.com")
+                    .withFirstName("Casey")
+                    .withLastName("Starrenburg")
+                    .build(),
+            Applicant.builder()
+                    .withId(2)
+                    .withEmail("CorneevanDoorne@teleworm.us")
+                    .withFirstName("Cornee")
+                    .withLastName("van Doorne")
+                    .build()
+    );
+    private List<Interviewer> interviewers = Arrays.asList(
+            Interviewer.builder()
+                    .withInterviewerId(1)
+                    .withFirstName("Cyrelle")
+                    .withLastName("IJsseldijk")
+                    .build(),
+            Interviewer.builder()
+                    .withInterviewerId(2)
+                    .withFirstName("Clazina")
+                    .withLastName("van Hoorn")
+                    .build()
+    );
+
+    private InterviewRepository setupRepo(Interview... interviews) {
+        List<Interview> list = Arrays.asList(interviews);
+        return new MockInterviewRepo(list);
+    }
+
+    private RemindParticipants setupUseCase(ReminderSender sender, Interview... interviews) {
+        List<Interview> list = Arrays.asList(interviews);
+        mockInterviewRepo = new MockInterviewRepo(list);
+        return new RemindParticipantsUseCase(mockInterviewRepo, sender);
+    }
+
+    private long nextId;
+
+    private Interview.InterviewBuilder interviewBuilder(long inHours, Applicant applicant,
+                                                        List<Interviewer> interviewers) {
+        return Interview.builder()
+                .withInterviewId(nextId++)
+                .withScheduledTime(LocalDateTime.now().plusHours(inHours))
+                .withCreatedTime(LocalDateTime.now().minusDays(5))
+                .withApplicant(applicant)
+                .withInterviewers(interviewers);
+    }
 
     @Before
-    private void setup() {
-        mockInterviewRepo = new MockInterviewRepo(Arrays.asList(
-                Interview.builder()
-                    .withInterviewId(1)
-                    .withCancelled(false)
-                    .withPreInterviewReminderSent(false)
-                    .withCreatedTime(LocalDateTime.now().minusDays(3))
-                    .withScheduledTime(LocalDateTime.now().plusHours(12))
-                    .withApplicant(Applicant.builder()
-                            .withEmail("test@test.com")
-                            .build())
-                    .withInterviewers(Arrays.asList(
-                        new Interviewer(1, "Maarten", "Billiet")
-                    ))
-                    .build()
-        ));
+    public void setup() {
+        nextId = 1;
     }
 
     @Test
     public void onlyInterviewsAfterCutoff() {
+        ReminderSender sender = mock(ReminderSender.class);
+        useCase = setupUseCase(
+                sender,
+                interviewBuilder(12, applicants.get(0), interviewers).build(),
+                interviewBuilder(50, applicants.get(0), interviewers).build(),
+                interviewBuilder(100, applicants.get(0), interviewers).build()
+        );
+        useCase.checkReminders();
+        verify(sender, times(1)).remindApplicant(any(), any());
     }
 
     @Test
-    public void ignoreInterviewsWithReminderAlreadySent() {
+    public void reminderSentFlagUpdated() {
+        ReminderSender sender = mock(ReminderSender.class);
+        useCase = setupUseCase(
+                sender,
+                interviewBuilder(12, applicants.get(0), interviewers).build()
+        );
+        Interview interview = mockInterviewRepo.findById(1).get();
+        assertThat(interview.isPreInterviewReminderSent(), is(false));
+        useCase.checkReminders();
+        interview = mockInterviewRepo.findById(1).get();
+        assertThat(interview.isPreInterviewReminderSent(), is(true));
+    }
 
+    @Test
+    public void ignoreCancelledInterviews() {
+
+        verifyNoRemindersSent();
+    }
+
+
+    @Test
+    public void ignoreInterviewsWithReminderAlreadySent() {
+        verifyNoRemindersSent();
     }
 
     @Test
     public void remindsApplicant() {
-
+        Applicant applicant = null;
+        ReminderSender sender = mock(ReminderSender.class);
+        verify(sender, times(1)).remindApplicant(applicant, any());
+        useCase.checkReminders();
     }
 
-    @Test
+    /*@Test
     public void remindsInterviewers() {
+        ReminderSender sender = mock(ReminderSender.class);
+        verify(sender, times(2)).remindApplicant(applicant, any());
+        useCase.checkReminders();
+    }*/
 
+    private void verifyNoRemindersSent() {
+        ReminderSender sender = mock(ReminderSender.class);
+        verify(sender, times(0)).remindApplicant(any(), any());
+        verify(sender, times(0)).remindInterviewer(any(), any());
+        useCase.checkReminders();
     }
+
 }
