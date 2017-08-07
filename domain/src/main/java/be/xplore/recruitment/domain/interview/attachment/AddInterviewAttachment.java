119fb237cbba4b771d97430403f7b0b9fad82b73
Stijn Schack <stijn.schack@student.kdg.be>
2017-08-07 13:14:14 +0200

    add interview attachment in domain

---
+++ domain/src/main/java/be/xplore/recruitment/domain/interview/attachment/AddInterviewAttachment.java
@@ -0,0 +1,15 @@
+package be.xplore.recruitment.domain.interview.attachment;
+
+import be.xplore.recruitment.domain.exception.CouldNotAddAttachmentException;
+import be.xplore.recruitment.domain.exception.NotFoundException;
+
+import java.util.function.Consumer;
+
+/**
+ * @author Stijn Schack
+ * @since 8/7/2017
+ */
+public interface AddInterviewAttachment {
+    void addAttachment(AddInterviewAttachmentRequest request, Consumer<InterviewAttachmentResponseModel> response)
+            throws NotFoundException, CouldNotAddAttachmentException;
+}
