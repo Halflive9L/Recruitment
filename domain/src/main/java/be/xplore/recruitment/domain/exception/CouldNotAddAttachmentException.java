bf97d4d6fd3f3f817deeb176422b68aa444bbb54
mkb <mkb@xd.xd>
2017-08-08 13:33:24 +0200

    Add Serial Version UID

--- domain/src/main/java/be/xplore/recruitment/domain/exception/CouldNotAddAttachmentException.java
+++ domain/src/main/java/be/xplore/recruitment/domain/exception/CouldNotAddAttachmentException.java
@@ -5,4 +5,5 @@ package be.xplore.recruitment.domain.exception;
  * @since 8/4/2017
  */
 public class CouldNotAddAttachmentException extends RuntimeException {
+    private static final long serialVersionUID = -5358760347754179762L;
 }
