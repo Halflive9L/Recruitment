c72b75d90952781dfcf4e041e1e939d8c08b887f
mkb <mkb@xd.xd>
2017-08-08 16:09:48 +0200

    Replace deprecated Mockito imports

--- domain/src/test/java/be/xplore/recruitment/domain/interviewer/CreateInterviewerTest.java
+++ domain/src/test/java/be/xplore/recruitment/domain/interviewer/CreateInterviewerTest.java
@@ -3,7 +3,7 @@ package be.xplore.recruitment.domain.interviewer;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
