98287aefb6b15dd9adb6073eb8d5d38f0edd2c50
Stijn Schack <stijn.schack@student.kdg.be>
2017-08-09 10:09:31 +0200

    replaced @Surpresswarnigs("serial") with serialVersionUID = 1L;

--- domain/src/main/java/be/xplore/recruitment/domain/exception/CouldNotDeleteAttachmentException.java
+++ domain/src/main/java/be/xplore/recruitment/domain/exception/CouldNotDeleteAttachmentException.java
@@ -4,8 +4,10 @@ package be.xplore.recruitment.domain.exception;
  * @author Stijn Schack
  * @since 8/8/2017
  */
-@SuppressWarnings("serial")
+
 public class CouldNotDeleteAttachmentException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
     public CouldNotDeleteAttachmentException(Throwable cause) {
         super(cause);
     }
