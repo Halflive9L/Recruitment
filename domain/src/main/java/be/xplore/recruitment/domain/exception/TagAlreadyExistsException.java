7c31682296de3052fafea2a3337cba579f3b54b1
Stijn Schack <stijn.schack@student.kdg.be>
2017-08-10 11:33:09 +0200

    added serialVersionUID

--- domain/src/main/java/be/xplore/recruitment/domain/exception/TagAlreadyExistsException.java
+++ domain/src/main/java/be/xplore/recruitment/domain/exception/TagAlreadyExistsException.java
@@ -7,7 +7,14 @@ import be.xplore.recruitment.domain.tag.Tag;
  * @since 8/10/2017
  */
 public class TagAlreadyExistsException extends RuntimeException {
+
+    private static final long serialVersionUID = 3602612837962159246L;
+
     public TagAlreadyExistsException(Tag tag, Throwable cause) {
         super(String.format("Tag with name: \"%s\" already exists", tag.getTagName()), cause);
+    }
+
+    public TagAlreadyExistsException(Tag tag) {
+        this(tag, null);
     }
 }
